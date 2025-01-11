d37e79bc25159b439baeb38c59d86236e54be735
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2025-01-11 11:57:10 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg'
 import './App.css'
 import Header from './components/Header'
-import {Routes, Route } from 'react-router-dom'
-import MovieSerh from './components/pages/Movie/Movie'
-import TrendTodey from './components/pages/TrendTodey/TrendTodey'
-import Loadpage from './components/pages/Loadpage/Loadpage'
-import Cast from "./components/pages/Loadpage/Cast"
-import Reviews from "./components/pages/Loadpage/Reviews"
+import { Routes, Route, Router } from 'react-router-dom'
+// import MovieSerh from './components/pages/Movie/Movie'
+// import TrendTodey from './components/pages/TrendTodey/TrendTodey'
+// import Loadpage from './components/pages/Loadpage/Loadpage'
+// import Cast from "./components/pages/Loadpage/Cast"
+// import Reviews from "./components/pages/Loadpage/Reviews"
+import React, { Suspense } from 'react'
+const Reviews = React.lazy(() => import('./components/pages/Loadpage/Reviews'))
+const Cast = React.lazy(() => import('./components/pages/Loadpage/Cast'))
+const Loadpage = React.lazy(() =>
+	import('./components/pages/Loadpage/Loadpage')
+)
+const TrendTodey = React.lazy(() =>
+	import('./components/pages/TrendTodey/TrendTodey')
+)
+const MovieSerh = React.lazy(() => import('./components/pages/Movie/Movie.jsx'))
 function App() {
 	return (
 		<>
 			<Header></Header>
-			<Routes>
-				<Route path="/" element={<TrendTodey />}></Route>
-				<Route path="movie" element={<MovieSerh />} />
-				{/* <Route path="/loadpage" element={<Loadpage />} /> */}
-				<Route path="/loadpage/:id" element={<Loadpage />}>
-					<Route path="cast/:id" element={<Cast />} />
-					<Route path="reviews/:id" element={<Reviews />} />
-				</Route>
-			</Routes>
+				<Suspense fallback={<div>Завантаження...</div>}>
+					<Routes>
+						<Route path="/" element={<TrendTodey />}></Route>
+						<Route path="movie" element={<MovieSerh />} />
+						<Route path="/loadpage/:id" element={<Loadpage />}>
+							<Route path="cast/:id" element={<Cast />} />
+							<Route path="reviews/:id" element={<Reviews />} />
+						</Route>
+					</Routes>
+				</Suspense>
 		</>
 	)
 }
